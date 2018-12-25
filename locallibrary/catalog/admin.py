48a92f614b2a52e01e0f8a2fa7f7ae462bdb2502
ManiGhahremani <mani.ghahremani@port.ac.uk>
2018-12-25 23:52:57 +0000

    25dec 2350

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -12,21 +12,52 @@ admin.site.register(Genre)
 #admin.site.register(Author)
 # Define the admin class
 
+class BookInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin) """
+    model=Book
+
 class AuthorAdmin(admin.ModelAdmin):
+    """Administration object for Author models.
+    Defines:
+    - fields to be displayed in list view (list_display)
+    - orders fields in detail view (fields), grouping the date fields horizontally
+    - adds inline addition of book in author view (inlines) [scroll to the bottom
+    of an author page to add books written by him/her]
+    """
+
     list_display=('last_name','first_name','date_of_birth','date_of_death')
     fields=['first_name','last_name', ('date_of_birth', 'date_of_death')]
+    inlines=[BooksInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
 
-# Register the Admin classes for Book using the decorator
-@admin.register(Book)
+class BookInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion used in (BookAdmin)"""
+    model = BookInstance
+
+
 class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models:
+    Defines:
+    - fields to be displayed in list view (list_display)
+    - adds inline addition of book instances in book view (inlines)
+    """
     list_display=('title','author','display_genre')
+    inlines = [BookInstanceInline]
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book, BookAdmin)
 
 # Register the Admin classes for BookInstace using the decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
+    """Administration object for BookInstace models.
+    Defines:
+    - fields to be displayed in list view (list_display)
+    - filters that will be displayed in sidebar (list_filter)
+    - grouping of fields into sections (fieldsets)
+    """
     list_display=('book','status','due_back','id')
     list_filter=('status', 'due_back')
 
